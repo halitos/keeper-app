9e83171dd3abd7842d1e857738eb0244416ea4ee
halit <oskan96@gmail.com>
2020-12-07 19:49:04 +0000

    used my API for sample note

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,56 +1,55 @@
-import React, {useState, useEffect} from 'react'
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
-import NoteInput from './NoteInput'
-
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import NoteInput from "./NoteInput";
 
 const App = () => {
   const [newNotes, setNewNotes] = useState([]);
+  const url = "https://halit-chat-server.glitch.me/messages";
 
   useEffect(() => {
-    fetch("https://halit-chat-server.glitch.me/messages")
-    .then(data => data.json())
-    .then((res) => {
-      console.log(res);
-      setNewNotes(prevNote => {
-        return [...prevNote, res[0]]
-      });
-    })
-    .catch(error => console.log(error));
-  }, [])
+    fetch(url)
+      .then((data) => data.json())
+      .then((res) => {
+        setNewNotes((prevNote) => {
+          return [...prevNote, ...res];
+        });
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-   const addNote = (note) =>{
-    setNewNotes(prevNote => {
-       return [...prevNote, note]
-     })
-   }
+  const addNote = (note) => {
+    setNewNotes((prevNote) => {
+      return [...prevNote, note];
+    });
+  };
 
-   const deleteNote = (id) =>{
-    setNewNotes(prevNote => {
-     return prevNote.filter((note, index) => index !== id)
-     })
-   } 
-   
-   return (
-        <div>
-        <Header/>
-        <NoteInput addNote={addNote}/>
-        {newNotes && newNotes.map((note, i)=>{
-         return(
-            <Note 
-            key={i}
-            id={i}
-            deleteNote={deleteNote}
-            title={note.title}
-            content={note.content}
+  const deleteNote = (id) => {
+    setNewNotes((prevNote) => {
+      return prevNote.filter((note, index) => index !== id);
+    });
+  };
+
+  return (
+    <div>
+      <Header />
+      <NoteInput addNote={addNote} />
+      {newNotes &&
+        newNotes.map((note, i) => {
+          return (
+            <Note
+              key={i}
+              id={i}
+              deleteNote={deleteNote}
+              title={note.title}
+              content={note.content}
             />
-         ) 
+          );
         })}
-        <Footer/>
-        </div>
-    )
-}
-
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- src/Components/NoteInput.js
+++ src/Components/NoteInput.js
@@ -1,62 +1,64 @@
-import React, {useState} from 'react'
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-import Zoom from '@material-ui/core/Zoom';
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
+const NoteInput = ({ addNote }) => {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
 
-const NoteInput = ({addNote}) => {
-const [note, setNote] = useState({
-    title: "", 
-    content : ""
-})
+  const [display, setDisplay] = useState(false);
 
-const [display, setDisplay] = useState(false);
+  const handlePopup = () => {
+    setDisplay(true);
+  };
 
-const handlePopup = () =>{
-setDisplay(true);
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  };
 
-const handleChange = (e) => {
-    const {name, value} = e.target;
-    setNote(prevNote => {
-       return {
-           ...prevNote,
-           [name]: value
-       }; 
-    })
-}
-
-const submitNote = (e) =>{
+  const submitNote = (e) => {
     e.preventDefault();
     note.title ? addNote(note) : alert("please fill before add");
     setNote({
-        title: "", 
-        content : ""
-    })
+      title: "",
+      content: "",
+    });
     setDisplay(false);
-}
-    return (
-        <form className="create-note">
-                {display && <input 
-                name="title" 
-                onChange={handleChange} 
-                value={note.title} 
-                placeholder="Title"
-            />}
-            <textarea 
-                onClick={handlePopup}
-                name="content" 
-                onChange={handleChange}
-                value={note.content} 
-                placeholder="Notes..."
-                rows={display ? "3" : "1"}
-            />
-            <Zoom in={display}>
-                <Fab onClick={submitNote} ><AddIcon/></Fab>
-            </Zoom>
-        </form>
-    )
-}
-
+  };
+  return (
+    <form className="create-note">
+      {display && (
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+      )}
+      <textarea
+        onClick={handlePopup}
+        name="content"
+        onChange={handleChange}
+        value={note.content}
+        placeholder={display ? "Notes" : "Enter new note..."}
+        rows={display ? "3" : "1"}
+      />
+      <Zoom in={display}>
+        <Fab onClick={submitNote}>
+          <AddIcon />
+        </Fab>
+      </Zoom>
+    </form>
+  );
+};
 
 export default NoteInput;
